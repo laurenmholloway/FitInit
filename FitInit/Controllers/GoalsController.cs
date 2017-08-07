77f2912c4621f1cf7349f4229dbe3d99a5fec7d4
Lauren Holloway <laurenholloway1010@gmail.com name = Lauren Holloway>
2017-08-07 17:47:19 -0400

    init

---
+++ FitInit/Controllers/GoalsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FitInit.Models;
+
+namespace FitInit.Controllers
+{
+    public class GoalsController : Controller
+    {
+        private FitInitEntities db = new FitInitEntities();
+
+        // GET: Goals
+        public ActionResult Index()
+        {
+            return View(db.Goals.ToList());
+        }
+
+        // GET: Goals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Goal goal = db.Goals.Find(id);
+            if (goal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goal);
+        }
+
+        // GET: Goals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Goals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GoalID,GoalName,GoalReps,GoalUser")] Goal goal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Goals.Add(goal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(goal);
+        }
+
+        // GET: Goals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Goal goal = db.Goals.Find(id);
+            if (goal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goal);
+        }
+
+        // POST: Goals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GoalID,GoalName,GoalReps,GoalUser")] Goal goal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(goal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(goal);
+        }
+
+        // GET: Goals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Goal goal = db.Goals.Find(id);
+            if (goal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goal);
+        }
+
+        // POST: Goals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Goal goal = db.Goals.Find(id);
+            db.Goals.Remove(goal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FitInit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitInit.Startup))]
+namespace FitInit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
